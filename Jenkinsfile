b4743b2dcf69f4f9cce548e4eb986571cc6d08d7
davidamar <davidamar1x@gmail.com>
2024-02-23 08:10:04 +0000

    Upload files to "/"

---
+++ Jenkinsfile
@@ -0,0 +1,100 @@
+properties([pipelineTriggers([cron('20 11,14,18 * * *')])])
+pipeline {
+    agent { label 'ubuntu' }
+
+    environment {
+        TOR_USER = credentials('TOR_USER')
+        TOR_PASS = credentials('TOR_PASS')
+        OTP_KEY = credentials('OTP_KEY')
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    def repoDir = 'TorrentVisitor'
+                    def repoExists = fileExists(repoDir)
+                    def pipInstalled = sh(script: 'command -v pip', returnStatus: true) == 0
+
+                    if (!repoExists) {
+                        echo "Cloning the Git repository..."
+                        sh 'git clone https://github.com/david1x/TorrentVisitor.git'
+                    } else {
+                        echo "Repository already exists. Removing existing repository and cloning a fresh copy."
+                        sh "rm -rf ${repoDir}"
+                        sh 'git clone https://github.com/david1x/TorrentVisitor.git'
+                    }
+                }
+            }
+        }
+
+        stage('Create Virtual Environment') {
+            steps {
+                script {
+                    dir('TorrentVisitor') {
+                        // Install pip if not already installed
+                        def pipInstalled = sh(script: 'command -v pip', returnStatus: true) == 0
+                        if (!pipInstalled) {
+                            sh 'sudo apt-get update && sudo apt-get install -y python3-pip'
+                        }
+    
+                        // Create a virtual environment in the TorrentVisitor directory
+                        sh 'python3 -m venv venv'
+    
+                        // Activate the virtual environment
+                        sh '. venv/bin/activate'
+    
+                        // Install requirements.txt within the virtual environment
+                        sh 'pip3 install -r requirements.txt'
+                    }
+                    
+                }
+            }
+        }
+
+        stage('Run') {
+            steps {
+                dir('TorrentVisitor') {
+                    script {
+                        // Use double quotes to interpolate variables
+                        sh "python3 main.py"
+                    }
+                    
+                }
+            }
+        }
+
+        stage('Cleanup') {
+            steps {
+                script {
+                    def repoDir = 'TorrentVisitor'
+                    sh 'pwd'
+                    echo "Deleting Running Folder..."
+                    sh "rm -rf ${repoDir}"
+                    sh "rm -rf ${repoDir}@tmp"
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                // Check if virtual environment is activated before deactivating
+                if (sh(script: '[[ -n "$VIRTUAL_ENV" ]]', returnStatus: true) == 0) {
+                    sh 'deactivate'
+                    echo 'Deactivated the virtual environment.'
+                } else {
+                    echo 'No virtual environment is currently activated. Skipping deactivation.'
+                }
+                cleanWs(cleanWhenNotBuilt: false,
+                        deleteDirs: true,
+                        disableDeferredWipeout: true,
+                        notFailBuild: true,
+                        patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                                   [pattern: '.propsfile', type: 'EXCLUDE']])
+            }
+        }
+    }
+}
+
---
+++ main.py
@@ -0,0 +1,164 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from datetime import datetime
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import logging
+import pyotp
+import time
+import os
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+load_dotenv()
+
+@dataclass
+class ChromeDriver:
+    """Data class for configuring ChromeDriver and Selenium."""
+    driver_path: any = ChromeDriverManager().install()
+    chrome_options: Options = None
+    selenium_grid_url: str = None
+    
+    def create_chrome_driver(self):
+        """Create a webdriver.Chrome instance with the specified configurations."""
+        logging.info('Loading Chrome Driver')
+        service = Service(self.driver_path)
+        return webdriver.Chrome(service=service, options=self.chrome_options)
+    
+    def is_driver_valid(self, driver):
+        logging.info('Validating Driver...')
+        if (driver.current_url):
+            logging.info('Driver is valid')
+            return True
+        else:
+            logging.error('Driver is not valid')
+            return False
+    
+    
+@dataclass
+class Website:
+    urls: list
+    title: str
+    elements: dict
+    driver: ChromeDriver = None
+    
+    def isBrowserAlive(self):
+        logging.info("Checking if browser is open")
+        if (self.driver.title == self.title):
+            logging.info("Browser is open")
+            return True
+        logging.error("Browser is not open")
+        return False
+
+
+@dataclass
+class Credential:
+    username: str
+    password: str
+    otp_key: str = None
+    driver: ChromeDriver = None
+    website: Website = None
+    
+    def otp_current_code(self, key):
+        return pyotp.TOTP(key).now()
+    
+    def enter_credentials(self):
+        logging.info('Entering Credentials')
+        user = self.driver.find_element(by="name", value="username")
+        password = self.driver.find_element(by="name", value="password")
+        user.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        
+        logging.info('Getting OTP Code for Login')
+        code = self.otp_current_code(self.otp_key)
+        self.driver.find_element(By.XPATH, self.website.elements['otp']).send_keys(code)
+        time.sleep(0.4)
+        self.driver.find_element(By.XPATH, self.website.elements['login-btn']).send_keys(Keys.RETURN)
+        logging.info(f'Current URL: {self.driver.current_url}')
+        time.sleep(5)
+        return
+
+    def validate_login(self):
+        logging.info('Checking if login passed')
+        status = self.driver.current_url
+        logging.info('status: ' + status)
+        time.sleep(5)
+        if 'torrents/top/index' not in status:
+            if (self.driver.find_element(By.XPATH, self.website.elements['otp-auth-error-check']).text):
+                logging.error("Authentication failed. Trying again... ")
+                self.enter_credentials()
+                return self.validate_login()
+        logging.info('Login passed')    
+        return True
+        
+
+def main() -> None:
+    logging.info('Starting Script')
+    logging.info('This script will run in headless mode')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging']) # Disable the DevTools console output
+    chrome_options.add_argument('--no-sandbox')  
+    chrome_options.add_argument('--disable-dev-shm-usage') 
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument('--headless')  # Add this line for headless mode
+    
+    chrome_driver = ChromeDriver(chrome_options=chrome_options)
+    driver = chrome_driver.create_chrome_driver()
+
+    if not (chrome_driver.is_driver_valid(driver)):
+        exit(1)
+
+    website = Website(
+        urls=[
+            'https://www.torrentleech.me/user/account/login/',
+            f'https://www.torrentleech.me/profile/{os.getenv("TOR_USER")}/achievements'
+        ],
+        title='TorrentLeech.org',
+        elements={
+            'otp':'//*[@id="prefcode"]/input[1]',
+            'login-btn':'//*[@id="site-canvas"]/div[2]/div/div/section/form/div[2]/button',
+            'otp-auth-error-check':'//*[@id="site-canvas"]/div[2]/div[1]/div/section/p'
+        },
+        driver=driver
+    )
+    
+    website.isBrowserAlive()
+    
+    logging.info('Loading credentials from environment variables')
+    creds = Credential(
+        username = os.getenv('TOR_USER'),
+        password = os.getenv('TOR_PASS'),
+        otp_key = os.getenv('OTP_KEY'),
+        driver = driver,
+        website = website
+    )
+     
+    logging.info(f'Loading URL: {website.urls[0]} to driver')
+    driver.get(website.urls[0])
+    logging.info(f'URL: {driver.current_url} loaded successfully')
+    
+    creds.enter_credentials()
+    creds.validate_login()
+  
+    if not (website.isBrowserAlive()):
+        logging.error("Driver was closed for unknown reason.")
+        logging.error("Terminating program.")
+        exit(1)
+        
+    logging.info(f'Loading Final URL: {website.urls[1]} to driver')
+    driver.get(website.urls[1])
+    logging.info(f'URL: {driver.current_url} loaded successfully')
+    
+    driver.refresh()
+    driver.quit()
+    logging.info("Program finished successfully!!!")
+    exit(0)
+        
+    
+if __name__ == "__main__":
+    main()
